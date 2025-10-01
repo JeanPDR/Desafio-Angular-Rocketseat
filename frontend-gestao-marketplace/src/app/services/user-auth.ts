7c28c8afb0b6634784d9cbf95926e8259680d099
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2025-09-30 22:11:03 -0300

    Implementa formulário de login com validação e tratamento de erros

--- frontend-gestao-marketplace/src/app/services/user-auth.ts
+++ frontend-gestao-marketplace/src/app/services/user-auth.ts
@@ -6,6 +6,10 @@ import { Injectable } from '@angular/core';
 export class UserAuthService {
   getUserToken(){
     //Recuperar token do Localstorage
-    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoibm92b0BleGFtcGxlLmNvbSIsImlhdCI6MTc1OTI3NTc4MiwiZXhwIjoxNzU5MzYyMTgyfQ.O93oywcLrKIuZztyko3iHGipOtgi9T_WrGsf1Qt81_U';
+    return localStorage.getItem('auth-token') || '';
+  }
+
+  setUserToken(token: string){
+    localStorage.setItem("auth-token", token);
   }
 }
--- frontend-gestao-marketplace/src/app/services/user.ts
+++ frontend-gestao-marketplace/src/app/services/user.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { IAuthSucessResponse } from '../interfaces/auth-sucess-response';
 import { Observable } from 'rxjs';
+import { ILoginSuccessResponse } from '../interfaces/login-sucess-response';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,11 @@ export class UserService {
 
   validateUser(): Observable<IAuthSucessResponse> {
     return this._httpClient.get<IAuthSucessResponse>('http://localhost:3000/api/protected');
+  }
+
+  login(email: string, password: string): Observable<ILoginSuccessResponse>{
+    const body = { email, password}
+    return this._httpClient.post<ILoginSuccessResponse>("http://localhost:3000/api/users/login", body)
+
   }
 }
