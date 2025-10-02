005f045a4965420dc15a2961b5af0c6a761ba24a
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2025-10-01 21:50:33 -0300

    realizando a criação dos filtros e listagem produtos e configurando formulario

--- frontend-gestao-marketplace/src/app/services/products.ts
+++ frontend-gestao-marketplace/src/app/services/products.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { INewProductRequest } from '../interfaces/new-product-request';
 import { Observable} from 'rxjs';
+import { IProductsResponse } from '../interfaces/products-response';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,10 @@ export class ProductsService {
   saveProduct(product: INewProductRequest): Observable<INewProductRequest>{
     return this._httpClient.post<INewProductRequest>('http://localhost:3000/api/products', product);
 
+  }
 
+  getProducts(): Observable<IProductsResponse>{
+    return this._httpClient.get<IProductsResponse>('http://localhost:3000/api/products');
   }
   
 }
